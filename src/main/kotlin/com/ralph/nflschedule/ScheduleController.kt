416f35548d580f342bc9b76c18cd8948fc960dcf
Ralph Mallett <rmallet6@ford.com>
2024-08-22 11:12:52 +0100

    add schedule and teams endpoints

--- src/main/kotlin/com/ralph/nflschedule/ScheduleController.kt
+++ src/main/kotlin/com/ralph/nflschedule/ScheduleController.kt
@@ -2,10 +2,21 @@ package com.ralph.nflschedule
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
-class ScheduleController{
+class ScheduleController(val scheduleService: ScheduleService) {
 
     @GetMapping("/test")
     fun test(): String = "Test endpoint!"
+
+    @GetMapping("/schedule")
+    fun getSchedule(): Mono<String> {
+        return scheduleService.getSchedule()
+    }
+
+    @GetMapping("/teams")
+    fun getTeams(): String {
+        return "team info"
+    }
 }
